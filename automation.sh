856104911d320e813dd853053a7a1236a7d97adf
avdesh <avdesh@email.com>
2021-07-17 10:13:15 +0000

    Invertory and cron changes

--- automation.sh
+++ automation.sh
@@ -33,5 +33,27 @@ else
   systemctl start apache2
 fi
 
+# Create inventory.html if does not exist
+if [[ ! -e /var/www/html/inventory.html ]]; then
+    touch /var/www/html/inventory.html
+    #echo "<html><head>Inventory</head><body>"
+    #echo "<th><td>Log Type</td><td>Time Created</td><td>Type</td><td>Size</td></th>" >> /var/www/html/inventory.html
+    echo "Log Type:Time Created:Type:Size" | tr : '\t\t' > /var/www/html/inventory.html
+fi
+
+
+#Zip and upload log files
 tar -czf /tmp/${myname}-httpd-logs-${timestamp}.tar /var/log/apache2/*.log
 aws s3 cp /tmp/${myname}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
+
+#Update Inventory
+filename="/tmp/${myname}-httpd-logs-${timestamp}.tar"
+filesize=$(stat -c%s "$filename")
+#echo $filesize
+#echo "<tr><td>httpd-logs</td><td>${timestamp}</td><td>tar</td><td>${filesize}</td></tr>" >> /var/www/html/inventory.html
+echo "httpd-logs:${timestamp}:tar:${filesize}" | tr : '\t\t' >> /var/www/html/inventory.html
+
+#Create cron job if does not exist
+if [[ ! -e /etc/cron.d/automation ]]; then
+    echo "0 0 * * * /root/Automation_Project/automation.sh && rm -rf /var/log/apache2/*.log" > /etc/cron.d/automation
+fi
